2deff088de12bc72ad01ec8a6841976a79ae00e7
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 08:40:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (number)
+  if ((number.remainder(5)==0)&&(number.remainder(3)==0)
+    return "Fizzbuzz"
+  elsif (number.remainder(5)==0)
+    return "Buzz"
+  elsif (number.remainder(3)==0)
+    return "Fizz"
+  else
+    return nil
+end
